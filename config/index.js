1e9374f4b6c2e9c7926bc32821bef05527a72831
Bogdan Keplast <bogdan.keplast@gmail.com>
2023-09-12 14:49:58 +0300

    update

--- config/index.js
+++ config/index.js
@@ -1,11 +1,6 @@
-const express = require('express');
 const mysql = require('mysql2/promise'); // Используем mysql2/promise для поддержки промисов
-const dbConfig = require('./database.js'); // Импортируем dbConfig из вашего модуля database.js
+const dbConfig = require('./database.js');
 
-const app = express();
-const port = 8000;
-
-// Создаем подключение к базе данных
 const pool = mysql.createPool({
     host: dbConfig.host,
     user: dbConfig.user,
@@ -13,94 +8,4 @@ const pool = mysql.createPool({
     database: dbConfig.database,
 });
 
-app.use(express.json());
-
-// Запрос для получения названий фильмов 
-// exxample - http://localhost:8000/films
-app.get('/films', async (req, res) => {
-    try {
-        const connection = await pool.getConnection();
-        const [rows, fields] = await connection.execute('SELECT title FROM film');
-        connection.release();
-        res.json(rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-// Запрос для получения названий фильмов определенного жанра
-// example http://localhost:8000/films-by-category/Action
-app.get('/films-by-category/:category', async (req, res) => {
-    try {
-        const category = req.params.category;
-        const [rows, fields] = await pool.execute('SELECT film.title FROM film INNER JOIN film_category ON film.film_id = film_category.film_id INNER JOIN category ON film_category.category_id = category.category_id WHERE category.name = ?', [category]);
-        res.json(rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-// Запрос для получения фильмов, в которых играет актер по указанному id
-// example http://localhost:8000/films-by-actor-id/1
-app.get('/films-by-actor-id/:actorId', async (req, res) => {
-    try {
-        const actorId = req.params.actorId;
-        const connection = await pool.getConnection();
-        const [rows, fields] = await connection.execute('SELECT film.title FROM film INNER JOIN film_actor ON film.film_id = film_actor.film_id WHERE film_actor.actor_id = ?', [actorId]);
-        connection.release();
-        res.json(rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-// Запрос для получения фильмов, в которых играет актер по указанной фамилии
-// example http://localhost:8000/films-by-actor-lastname/CHASE
-app.get('/films-by-actor-lastname/:lastName', async (req, res) => {
-    try {
-        const lastName = req.params.lastName;
-        const connection = await pool.getConnection();
-        const [rows, fields] = await connection.execute('SELECT film.title FROM film INNER JOIN film_actor ON film.film_id = film_actor.film_id INNER JOIN actor ON film_actor.actor_id = actor.actor_id WHERE actor.last_name = ?', [lastName]);
-        connection.release();
-        res.json(rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-// Запрос для получения фильмов, в которых играют актеры, у которых фамилия начинается с определенной буквы
-// example http://localhost:8000/films-by-actor-lastname-starting-with/G
-app.get('/films-by-actor-lastname-starting-with/:letter', async (req, res) => {
-    try {
-        const letter = req.params.letter;
-        const connection = await pool.getConnection();
-        const [rows, fields] = await connection.execute('SELECT film.title FROM film INNER JOIN film_actor ON film.film_id = film_actor.film_id INNER JOIN actor ON film_actor.actor_id = actor.actor_id WHERE actor.last_name LIKE ?', [letter + '%']);
-        connection.release();
-        res.json(rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-// Запрос для получения количества фильмов, сгруппированных по жанру
-// example http://localhost:8000/films-count-by-category
-app.get('/films-count-by-category', async (req, res) => {
-    try {
-        const [rows, fields] = await pool.execute('SELECT category.name, COUNT(film.film_id) AS film_count FROM category INNER JOIN film_category ON category.category_id = film_category.category_id INNER JOIN film ON film_category.film_id = film.film_id GROUP BY category.name');
-        res.json(rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,1 +1,108 @@
-require('/category/index.js');
+const express = require('express');
+const app = express();
+const pool = require("./config"); // Импортируйте pool из вашего конфигурационного файла
+
+const port = 8000;
+
+app.get('/categories', (req, res) => {
+    db.query('SELECT * FROM category', (error, result) => {
+        if(error) throw error
+        return res.send({ result })
+    })
+})
+
+
+// Создаем подключение к базе данных
+
+
+app.use(express.json());
+
+// Запрос для получения названий фильмов 
+// exxample - http://localhost:8000/films
+app.get('/films', async (req, res) => {
+    try {
+        const connection = await pool.getConnection();
+        const [rows, fields] = await connection.execute('SELECT title FROM film');
+        connection.release();
+        res.json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Запрос для получения названий фильмов определенного жанра
+// example http://localhost:8000/films-by-category/Action
+app.get('/films-by-category/:category', async (req, res) => {
+    try {
+        const category = req.params.category;
+        const [rows, fields] = await pool.execute('SELECT film.title FROM film INNER JOIN film_category ON film.film_id = film_category.film_id INNER JOIN category ON film_category.category_id = category.category_id WHERE category.name = ?', [category]);
+        res.json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Запрос для получения фильмов, в которых играет актер по указанному id
+// example http://localhost:8000/films-by-actor-id/1
+app.get('/films-by-actor-id/:actorId', async (req, res) => {
+    try {
+        const actorId = req.params.actorId;
+        const connection = await pool.getConnection();
+        const [rows, fields] = await connection.execute('SELECT film.title FROM film INNER JOIN film_actor ON film.film_id = film_actor.film_id WHERE film_actor.actor_id = ?', [actorId]);
+        connection.release();
+        res.json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Запрос для получения фильмов, в которых играет актер по указанной фамилии
+// example http://localhost:8000/films-by-actor-lastname/CHASE
+app.get('/films-by-actor-lastname/:lastName', async (req, res) => {
+    try {
+        const lastName = req.params.lastName;
+        const connection = await pool.getConnection();
+        const [rows, fields] = await connection.execute('SELECT film.title FROM film INNER JOIN film_actor ON film.film_id = film_actor.film_id INNER JOIN actor ON film_actor.actor_id = actor.actor_id WHERE actor.last_name = ?', [lastName]);
+        connection.release();
+        res.json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Запрос для получения фильмов, в которых играют актеры, у которых фамилия начинается с определенной буквы
+// example http://localhost:8000/films-by-actor-lastname-starting-with/G
+app.get('/films-by-actor-lastname-starting-with/:letter', async (req, res) => {
+    try {
+        const letter = req.params.letter;
+        const connection = await pool.getConnection();
+        const [rows, fields] = await connection.execute('SELECT film.title FROM film INNER JOIN film_actor ON film.film_id = film_actor.film_id INNER JOIN actor ON film_actor.actor_id = actor.actor_id WHERE actor.last_name LIKE ?', [letter + '%']);
+        connection.release();
+        res.json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Запрос для получения количества фильмов, сгруппированных по жанру
+// example http://localhost:8000/films-count-by-category
+app.get('/films-count-by-category', async (req, res) => {
+    try {
+        const [rows, fields] = await pool.execute('SELECT category.name, COUNT(film.film_id) AS film_count FROM category INNER JOIN film_category ON category.category_id = film_category.category_id INNER JOIN film ON film_category.film_id = film.film_id GROUP BY category.name');
+        res.json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
